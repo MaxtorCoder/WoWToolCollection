722c581b4ff0a588fc52735cd9448da10527cd53
MaxtorCoder <warsongkiller.s8@gmail.com>
2019-05-31 12:25:34 +0200

    Added DB2Dumper, sort off Changed AgentTest to download and install Classic Beta

--- AgentTest/AgentTest.cs
+++ AgentTest/AgentTest.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
+using System.IO;
 using System.Net.Http;
 using System.Threading;
 
@@ -12,30 +13,30 @@ namespace AgentTest
         {
             var client = new HttpClient();
             client.DefaultRequestHeaders.Add("User-Agent", "phoenix-agent/1.0");
-
+            
             var authResponse = client.GetStringAsync("http://127.0.0.1:1120/agent").Result;
             dynamic task = JObject.Parse(authResponse);
             client.DefaultRequestHeaders.Add("Authorization", (string)task.authorization);
-
+            
             Install install = new Install
             {
-                InstructiosDataSet = new string[] { "torrent", "win", "w3", "enus" },
-                InstructionsPatch = "http://eu.patch.battle.net:1119/w3",
+                InstructiosDataSet = new string[] { "torrent", "win", "wow_classic_beta", "enus" },
+                InstructionsPatch = "http://us.patch.battle.net:1119/wow_classic_beta",
                 InstructionProduct = "NGDP",
-                MonitorPID = 13892,
+                MonitorPID = 12345,
                 Priority = new Priority
                 {
                     InsertAtHead = false,
                     Value = 900
                 },
-                UID = "w3"
+                UID = "wow_classic_beta"
             };
             
             InstallBeta installBeta = new InstallBeta
             {
                 AccountCountry = "NLD",
                 Finalized = true,
-                GameDir = @"C:\\Program Files (x86)\\Warcraft 3",
+                GameDir = @"C:\\Program Files (x86)\\World of Warcraft\\World of Warcraft Public Test\\",
                 GeoIPCountry = "NL",
                 Language = new string[] { "enUS" },
                 SelectedAsset = "enUS",
@@ -51,29 +52,18 @@ namespace AgentTest
                     InsertAtHead = false,
                     Value = 699
                 },
-                UID = "w3"
+                UID = "wow_classic_beta"
             };
-
-            Uninstall uninstall = new Uninstall
+            
+            Console.WriteLine(client.PostAsync("http://127.0.0.1:1120/install", new StringContent(JsonConvert.SerializeObject(install))).Result);
+            
+            while(true)
             {
-                RunCompaction = true,
-                UID = "w3"
-            };
+                Console.WriteLine(client.PostAsync("http://127.0.0.1:1120/install/wow_classic_beta", new StringContent(JsonConvert.SerializeObject(installBeta))).Result);
+                Console.WriteLine(client.PostAsync("http://127.0.0.1:1120/update", new StringContent(JsonConvert.SerializeObject(update))).Result);
             
-            var Install     = new StringContent(JsonConvert.SerializeObject(install, Formatting.Indented));
-            var InstallBeta = new StringContent(JsonConvert.SerializeObject(installBeta, Formatting.Indented));
-            var Update      = new StringContent(JsonConvert.SerializeObject(update, Formatting.Indented));
-            var Uninstall   = new StringContent(JsonConvert.SerializeObject(uninstall, Formatting.Indented));
-            
-            Console.WriteLine(client.PostAsync("http://127.0.0.1:1120/uninstall", Uninstall).Result);
-            
-            // while(true)
-            // {
-            //     Console.WriteLine(client.PostAsync("http://127.0.0.1:1120/install/w3", InstallBeta).Result);
-            //     Console.WriteLine(client.PostAsync("http://127.0.0.1:1120/update", Update).Result);
-            // 
-            //     Thread.Sleep(2000);
-            // }
+                Thread.Sleep(2000);
+            }
         }
     }
 }
---
+++ DB2Dumper/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DB2Dumper
+{
+    class Program
+    {
+        public static Dictionary<string, string> DBFileEntries = new Dictionary<string, string>();
+        static void Main(string[] args)
+        {
+            PopulateDict();
+            string path = @"D:\Private Servers\World of Warcraft\CASC\work\dbfilesclient";
+            string[] files = Directory.GetFiles(path);
+
+            using (var writer = new StreamWriter("dumped_dbfilesclient.txt"))
+            {
+                foreach (var file in files)
+                {
+                    string fileName = Path.GetFileName(file).Replace("_", "");
+                    string entry = DBFileEntries[fileName];
+
+                    writer.WriteLine($"public static DB2Storage<{entry}Record> {entry}Storage;");
+                }
+            }
+        }
+
+        public static void PopulateDict()
+        {
+            using (var reader = new StreamReader("DBFilesClient.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var split = line.Split(';');
+
+                    DBFileEntries.Add(split[0], split[1]);
+                }
+            }
+        }
+    }
+}
